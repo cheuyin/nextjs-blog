881f98d41fbd8d45aeb6fe3ea08e905d734ed2c2
cheuyin <yinstanleycheung@gmail.com>
2023-01-05 18:05:05 -0800

    Fix posts.tsx import

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Date from "../components/date";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
-import { getSortedPostsData } from "../lib/posts.tsx";
+import { getSortedPostsData } from "../lib/posts";
 import { GetStaticProps } from "next";
 
 export const getStaticProps: GetStaticProps = async () => {
