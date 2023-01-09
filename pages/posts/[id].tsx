9f5a9793fa422e38d1755aafc6b5e0c1e7f0fef0
cheuyin <yinstanleycheung@gmail.com>
2023-01-08 21:16:31 -0800

    Convert individual post page into tailwind

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,7 +2,6 @@ import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
 import Date from "../../components/date";
-import utilStyles from "../../styles/utils.module.css";
 import { GetStaticProps, GetStaticPaths } from "next";
 
 export default function Post({
@@ -21,8 +20,8 @@ export default function Post({
 			</Head>
 
 			<article>
-				<h1 className={utilStyles.headingX1}>{postData.title}</h1>
-				<div className={utilStyles.lightText}>
+				<h1 className="text-3xl my-3 font-bold">{postData.title}</h1>
+				<div className="text-gray-400 mb-3">
 					<Date dateString={postData.date} />
 				</div>
 				<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
